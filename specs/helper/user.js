c4952a836d024e7315b4c2fac93fdbe589252a31
One-Fish-One-Bee <devourersoul2@yandex.ru>
2023-10-24 13:49:05 +0300

    Практический урок

---
+++ specs/helper/user.js
@@ -0,0 +1,37 @@
+import supertest from "supertest";
+
+import config from "../../config";
+const { url } = config
+
+let token = ""
+
+//контроллер user 
+
+const user = {
+    login: (payload) => {
+        return supertest(url)
+            .post("/api/v1/login")
+            .set("Accept", "application/json")
+            .send(payload)
+    },
+
+    async getAuthToken() {
+        const payload = config.credentials
+        const res = await this.login(payload)
+        return res.body.token
+    },
+
+    async getAuthTokenCache() {
+        token = await this.getAuthToken;
+        return token;
+    },
+
+    user: (token) => {
+        return supertest(url)
+            .get("api/v1/user")
+            .set("Accept", "application/json")
+            .set("Authorization", `Bearer ${token}`)
+    }
+}
+
+export default user
